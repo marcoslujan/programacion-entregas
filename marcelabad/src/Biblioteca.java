a8e3a6bbec533c2a722b4da18dc58d14a70c9f27
MarcelAbad <marcel.abad10@gmail.com>
2025-10-17 20:27:01 +0200

    Ejercicio Marcel Biblioteca

---
+++ marcelabad/src/Biblioteca.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Biblioteca {
+    MyScanner scanner = new MyScanner();
+    private int id;
+    private String nombre;
+    private ArrayList<Libro> libros;
+
+    public Biblioteca() {
+        this.libros = new ArrayList<>();
+    }
+
+    public Biblioteca(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.libros = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+
+    public void addLibro(Libro libro) {
+        this.libros.add(libro);
+    }
+
+    public void prestarLibro(Libro libro) {
+        libro.setDisponible(false);
+    }
+
+    public void devolverLibro(Libro libro) {
+        libro.setDisponible(true);
+    }
+
+    @Override
+    public String toString() { //
+        return "Biblioteca{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", libros=" + libros +
+                '}';
+    }
+}
