9a8db29ddd356788110f20e4d1521cf795082f99
CARLOS SUQUILANDA <carlos.suquilanda.2307@gmail.com>
2025-10-22 09:58:20 +0200

    Biblioteca

---
+++ carlossuquilanda/src/clases/MyScanner.java
@@ -0,0 +1,71 @@
+package carlossuquilanda.src.clases;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MyScanner {
+    private static Scanner sc;
+
+    public MyScanner() {
+        sc = new Scanner(System.in);
+    }
+
+    public static int pedirNumero(String mns) {
+        int n = -1;
+        boolean flag = true;
+        while (flag) {
+            try {
+                System.out.print(mns);
+                n = sc.nextInt();
+                sc.nextLine();
+                flag = false;
+            } catch (InputMismatchException e) {
+                System.out.println("\n❌Eso no es número❌");
+                System.out.println("🔢Por favor ingrese un numero🔢\n");
+                sc.nextLine();
+            }
+        }
+        return n;
+    }
+
+    public String pedirSoloTexto(String texto) {
+        String input;
+        boolean valido;
+        do {
+            System.out.println(texto);
+            input = sc.nextLine().trim();
+            valido = input.matches("[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ ]+");
+            if (!valido) {
+                System.out.println("ERROR: solo se permiten letras (sin números ni símbolos). Inténtalo de nuevo.");
+            }
+        }  while (!valido);
+        return input;
+    }
+
+    public char pedirLetra(String texto) {
+        String input;
+        boolean valido;
+        do {
+            System.out.println(texto);
+            input = sc.nextLine().trim();
+            valido = input.matches("[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ ]");
+            if (!valido) {
+                System.out.println("ERROR: solo se permite introducir una letra. Inténtalo de nuevo.");
+            }
+        }while (!valido);
+
+        return input.charAt(0);
+    }
+
+    public String pideTexto(String mensaje) {
+        String texto;
+        do {
+            System.out.println(mensaje);
+            texto = sc.nextLine();
+            if (texto.isEmpty()) {
+                System.out.println("Error: el campo no puede estar vacio.");
+            }
+        } while (texto.isEmpty());
+        return texto;
+    }
+
+}
