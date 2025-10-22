2c721b76b9da3c464fd733dbb0d2f0ceafc24a4f
pablo-gd <pablogardu21@gmail.com>
2025-10-22 08:58:18 +0200

    Te dejo el gestor de biblioteca con las clases que me faltaban

---
+++ pablo garcia duque/src/Libro/Libro.java
@@ -0,0 +1,87 @@
+package Libro;
+
+public class Libro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+    private String ISBN;
+    private boolean disponible;
+
+    public Libro() {
+        this.disponible = true;
+    }
+
+    public Libro(int id, String titulo, String autor, int numeroPaginas, String ISBN) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+        this.ISBN = ISBN;
+        this.disponible = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                ", ISBN='" + ISBN + '\'' +
+                ", disponible=" + disponible +
+                '}';
+    }
+}
+
+
+
+
